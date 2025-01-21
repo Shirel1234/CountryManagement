e7208dfa50ce4d0fa01b603a764b283e373fdf9f
Shirel Zohar <shirel.zohar@appricots.onmicrosoft.com>
2025-01-21 15:27:08 +0200

    second commit

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,16 +3,25 @@ import dotenv from 'dotenv';
 import countryRoutes from './routes/countryRoutes';
 import connect from './lib/db/mongodb';
 import path from 'path';
+import cors from 'cors'; 
 
 // Load environment variables from .env file
 dotenv.config({ path: path.resolve(__dirname, '../src/config/.env') });
 
 // Initialize the Express app
 const app = express();
-
+// Middleware for CORS
+app.use(
+  cors({
+    origin: 'http://localhost:3000', // Allow requests from this origin (Frontend)
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+  })
+);
 const startServer = async () => {
   try {
     await connect();
+    
     // Middleware for parsing JSON bodies
     app.use(express.json());
 
